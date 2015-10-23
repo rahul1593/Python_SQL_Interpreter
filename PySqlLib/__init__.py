a09d4ff1b0e3645ca28ae06f8450d49c374d2753
Rahul Bhartari <engineerbhartarisingh@gmail.com>
2015-10-23 17:52:11 +0530

    Create __init__.py

---
+++ PySqlLib/__init__.py
@@ -0,0 +1,10 @@
+#
+#    Core library intended to be used by the SQL Interpreter
+#    This Library contains the following modules
+#        __ common: contains the common functions to be used by all other modules
+#        __ create: functions to be used by the CREATE command processor.
+#        __ insert: functions to be used by the INSERT command processor.
+#        __ select: functions to be used by the CREATE command processor.
+#        __ update: functions to be used by the INSERT command processor.
+#        __ delete: functions to be used by the CREATE command processor.
+#
