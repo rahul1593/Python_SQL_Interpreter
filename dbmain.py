4f8fa3fc0bf5e35aa7632864a14840d4be0917ed
Rahul Bhartari <engineerbhartarisingh@gmail.com>
2015-10-25 20:40:03 +0530

    Update dbmain.py

--- dbmain.py
+++ dbmain.py
@@ -6,6 +6,7 @@ Created on Oct 4, 2015
 import sys
 from PySqlLib.constdata import *
 from PySqlLib.parser import *
+from PySqlLib.session_man import *
 
 if __name__ == '__main__':
     pass
@@ -15,9 +16,40 @@ if __name__ == '__main__':
 class dbmain(pySQL):
     def __init__(self):
         self.console = pyParser()
-
+        self.session = None
+    '''
+    process_cmdstk:
+    This method processes all the commands and passes the stack to the corresponding individual command processors
+    '''
     def process_cmdstk(self,stk):
-        return self.cmd_codes.EXIT
+        cmd = self.console.get_command(stk.pop())
+        if cmd == self.cmd_codes.EXIT:
+            return self.cmd_codes.EXIT
+        if 0<=cmd<=self.cmd_codes.code_max:
+            if cmd == self.cmd_codes.LOGIN:
+                self.session = Session(None)
+                return self.session.login(stk)
+            
+            elif self.session != None:
+                if cmd == self.cmd_codes.CREATE:
+                    pass
+                elif cmd == self.cmd_codes.DROP:
+                    pass
+                elif cmd == self.cmd_codes.ALTER:
+                    pass
+                elif cmd == self.cmd_codes.TRUNCATE:
+                    pass
+                elif cmd == self.cmd_codes.LOGIN:
+                    pass
+                elif cmd == self.cmd_codes.LOGIN:
+                    pass
+                elif cmd == self.cmd_codes.LOGIN:
+                    pass
+            else:
+                print("Login First !!")
+                return self.cmd_codes.ERR_UNAUTHORIZED
+        pass#to be completed
+
 
     def start(self):
         # main code starts here
@@ -25,11 +57,17 @@ class dbmain(pySQL):
         while True:
             tcmd=self.console.get_input()
             cstk=self.console.create_stack(tcmd)
-            if self.process_cmdstk(cstk)==self.cmd_codes.EXIT:
+            cstk.display()
+            ret=self.process_cmdstk(cstk)
+            if ret==self.cmd_codes.EXIT:
                 break
         print("Exiting ^_^")
     
 # start the command parser in continuous while loop
 session = dbmain()
 session.start()
+
+
+
+
 
